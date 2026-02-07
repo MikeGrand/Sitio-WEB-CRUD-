62d7e527b0f85caed625cffb4575a745f7f98c46
MikeGrand <jmikegrande@gmail.com>
2026-02-06 23:00:08 -0600

    Add files via upload

---
+++ sql.php
@@ -0,0 +1,24 @@
+<?php
+class sql 
+{
+    public $conn;
+
+    public function __construct()
+    {
+        $user = "root";
+        $pass = "";
+        $serv = "localhost";
+        $db ="articulo";
+
+        $this->conn = new mysqli($serv,$user,$pass,$db);
+
+    }
+
+    public function ejecutar($sql)
+    {
+        //echo $sql."<br>";
+        $result = $this->conn->query($sql);
+        return $result;
+    }
+}
+?>
